052b5fe2bb7ac8fb8129f5db2739cefea16a0ee1
SlarkleZ <54305079+SlarkleZ@users.noreply.github.com>
2020-08-10 21:59:01 +0700

    change tfidf output format

--- imdb/BOW/tfidf.py
+++ imdb/BOW/tfidf.py
@@ -4,25 +4,29 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 tfidf_word_value = []
 
 readIn = pd.read_csv("../Clean/lemma_result.csv")
-sample = readIn.head(100)
+sample = readIn.head(1000)
 
 all_review_cleaned = sample['cleaned_review'].astype('string')
 all_label = sample['Label'].astype('string')
 all_review_cleaned.drop(all_review_cleaned.head(0))
 
-#TF-IDF
+# TF-IDF
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(all_review_cleaned)
-#print(X.shape)
-tfidf_allword_value = list(zip(vectorizer.get_feature_names(), X.sum(0).getA1()))
-print("{0:20}{1:20}".format("Word", "result"))
 
-for word in tfidf_allword_value:
-    print(word)
+test = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names())
+test.to_csv("./tfidf_result.csv", index=False)
 
-print(X.toarray())
 
-data = {'Word': vectorizer.get_feature_names(), 'Weight': X.sum(0).getA1()}
-toFile = pd.DataFrame(data)
-toFile.to_csv("./tfidf_result.csv", index=False)
+# print(X.shape)
+#tfidf_allword_value = list(zip(vectorizer.get_feature_names(), X.sum(0).getA1()))
+#print("{0:20}{1:20}".format("Word", "result"))
 
+# for word in tfidf_allword_value:
+#    print(word)
+
+# print(X.toarray())
+
+#data = {'Word': vectorizer.get_feature_names(), 'Weight': X.sum(0).getA1()}
+#toFile = pd.DataFrame(data)
+#toFile.to_csv("./tfidf_result.csv", index=False)
--- imdb/Clean/lemma.py
+++ imdb/Clean/lemma.py
@@ -1,30 +1,31 @@
-import nltk
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-from nltk import word_tokenize
-from nltk.tokenize import RegexpTokenizer
-import re
-import pandas as pd
 import itertools
+import pandas as pd
+import re
+from nltk.tokenize import RegexpTokenizer
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import nltk
+nltk.download('wordnet')
 wordnet_lemmatizer = WordNetLemmatizer()
 reSentence = []
 allLabel = []
-#print(stopwords.words('english'))
-#print("--------------------------------------")
+# print(stopwords.words('english'))
+# print("--------------------------------------")
 
 # bring in sentence from file
 readIn = pd.read_csv("IMDB Dataset.csv")
-sample = readIn.head(100)
+sample = readIn.head(1000)
 InFilter = sample['review'].astype('string')
 Label = sample['sentiment'].astype('string')
 InFilter.drop(InFilter.head(0))
-print(InFilter)
+# print(InFilter)
 
 
 for label in Label:
     allLabel.append(label)
 
-print(allLabel)
+# print(allLabel)
 #sentence = "One of the other reviewers has mentioned that after watching just 1 Oz episode you'll be hooked. They are right, as this is exactly what happened with me.<br /><br />The first thing that struck me about Oz was its brutality and unflinching scenes of violence, which set in right from the word GO. Trust me, this is not a show for the faint hearted or timid. This show pulls no punches with regards to drugs, sex or violence. Its is hardcore, in the classic use of the word.<br /><br />It is called OZ as that is the nickname given to the Oswald Maximum Security State Penitentary. It focuses mainly on Emerald City, an experimental section of the prison where all the cells have glass fronts and face inwards, so privacy is not high on the agenda. Em City is home to many..Aryans, Muslims, gangstas, Latinos, Christians, Italians, Irish and more....so scuffles, death stares, dodgy dealings and shady agreements are never far away.<br /><br />I would say the main appeal of the show is due to the fact that it goes where other shows wouldn't dare. Forget pretty pictures painted for mainstream audiences, forget charm, forget romance...OZ doesn't mess around. The first episode I ever saw struck me as so nasty it was surreal, I couldn't say I was ready for it, but as I watched more, I developed a taste for Oz, and got accustomed to the high levels of graphic violence. Not just violence, but injustice (crooked guards who'll be sold out for a nickel, inmates who'll kill on order and get away with it, well mannered, middle class inmates being turned into prison bitches due to their lack of street skills or prison experience) Watching Oz, you may become comfortable with what is uncomfortable viewing....thats if you can get in touch with your darker side."
 
 
@@ -59,6 +60,3 @@ for sentence in InFilter:
 data = {'cleaned_review': reSentence, 'Label': allLabel}
 toFile = pd.DataFrame(data)
 toFile.to_csv("./lemma_result.csv", index=False)
-
-
-    
