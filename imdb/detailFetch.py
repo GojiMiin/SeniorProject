96e4d6bc33367535572bfb6a8d943cfdca68b97b
GojiMiin <got.j@windowslive.com>
2021-01-07 00:30:41 +0700

    fetch movie detail from 2019

--- imdb/detailFetch.py
+++ imdb/detailFetch.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from urllib import request
 from bs4 import BeautifulSoup
 import re
@@ -9,6 +8,11 @@ movieSource = []
 movieYear = []
 movieTime = []
 movieRate = []
+movieCate = []
+moviePoster = []
+movieDesciption = []
+clean_des = []
+movieScore = []
 
 all_url = "https://www.imdb.com/list/ls022596827/" #2019
 openn = request.urlopen(all_url).read().decode('utf8')
@@ -35,15 +39,47 @@ for i in range(len(movieBlock)):
     #get movie rate
     rate = movieBlock[i].find('span',{'class':'certificate'})
 
+    #get movie category
+    cate = movieBlock[i].find('span',{'class':'genre'}).contents[0]
+
+    #get movie poster link
+    plink = movieCodeLocation[i].find('img',{'class':'loadlate'}).get('loadlate')
+
+    #get movie description
+    des = movieBlock[i].find('p',{'class':''}).contents
+
+    #get movie score
+    score = movieBlock[i].find('span',{'class':'ipl-rating-star__rating'}).contents[0]
+
     movieName.append(str(name))
     movieSource.append("imdb")
     movieCode.append(str(code))
     movieYear.append(yearNum[0])
     movieTime.append(time)
-    if(rate != None):
-        clearRate = re.sub(r'/<\/?span[^>]*>/g',"", str(rate))
+
+    #Clean movie rate
+    if(str(rate) != "None"):
+        clearRate = re.sub("<[^>]*>","", str(rate))
         movieRate.append(clearRate)
     else:
         movieRate.append("None")
 
-print(movieRate)
+    #Clean movie category
+    del_n = re.sub("\n", "", str(cate))
+    movieCate.append(del_n.rstrip())
+
+    moviePoster.append(plink)
+
+    #Clean movie description
+    for i in des:
+        des_del_n = re.sub("\n","", str(i))
+        des_del_link = re.sub("<[^>]*>","", str(des_del_n))
+        clean_des.append(des_del_link)
+
+    full_sentence = " ".join(clean_des)
+    clean_des = []
+    movieDesciption.append(full_sentence.strip())
+    movieScore.append(score)
+
+
+print(movieScore)
